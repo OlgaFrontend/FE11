9dff5c8bc42a7d04c9cde65b2628a28bd13cccfd
OlgaFrontend <odnews10@gmail.com>
2017-12-02 02:00:34 +0200

    js_module8

---
+++ JS_module8/main.js
@@ -0,0 +1,70 @@
+const form = document.getElementById('form');
+const moviesList =document.querySelector('.movies-list');
+const input = document.getElementById('input');
+const searchBtn = document.getElementById('search-form__btn');
+
+const endpoint = 'https://api.themoviedb.org/3/';
+const res = 'search/movie';
+const apiKey = 'f24a0fd18f52218851075901c5a108a0';
+const apiUrl = `${endpoint}${res}?api_key=${apiKey}`;
+
+const fetchMovies = (searchQuery) =>
+	fetch(apiUrl + `&query=${searchQuery}`)
+		.then(response => {
+			if (response.ok) {
+				return response.json();
+			}
+
+			throw new Error (
+				'error while fetching: ' + response.statusText
+			);
+			
+		})
+		.then(data => data.results)
+		.catch(error => console.log('error'))
+	
+
+const renderGallery = (movies, parent) => {
+	let htmlString = '';
+	movies.forEach(movie => {
+		let overview = movie.overview.slice(0,99);
+        htmlString += `<div class="movie-list__item">
+        <div class="movie-card">
+        <span class="movie-card__rating">${movie.vote_average}</span>
+        <img  class="movie-card__img" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="movie-poster">
+        <div class="movie-info"
+	        <h3 class="movie-info__title">${movie.title}</h3>
+	        <p class="movie-info__overview">${overview}...</p>
+	        <p class="movie-info__release-date">${movie.release_date}</p>
+	    </div>
+        </div>
+        </div>`;
+	});
+	parent.innerHTML= htmlString;
+}
+
+const getMoviesGallery = (data) =>
+    data.map(item => ({
+  		poster_path: item.poster_path,
+        title: item.title,
+        release_date: item.release_date,
+        overview: item.overview.slice(0,99),
+        vote_average: item.vote_average
+  })
+);
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  
+    if (input.value != '') {
+    	fetchMovies(input.value)
+    	.then(data => {
+			const movies = getMoviesGallery(data);
+			renderGallery(movies, moviesList);
+		});
+		fetchMovies(input.value);
+		input.value = "";
+    } else {
+  		alert('Please enter a name of the movie');
+    }
+});
